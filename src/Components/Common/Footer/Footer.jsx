e26795a812601d2d185ef36b8e2dd3dcd782dc4a
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2025-05-27 23:58:50 +0600

    Navbar done.

---
+++ src/Components/Common/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            {/* image er moto same dsign deo pls  */}
+        </div>
+    );
+};
+
+export default Footer;
--- src/Layout/Mainlayout.jsx
+++ src/Layout/Mainlayout.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Components/Common/Navbar/Navbar';
+import Footer from '../Components/Common/Footer/Footer';
 
 const Mainlayout = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (isLoading) {
+        return (
+            <div className="h-screen flex items-center justify-center bg-white text-black ">
+                <span className="loading loading-bars loading-lg"></span>
+            </div>
+        );
+    }
     return (
-        <div>
+        <div className='w-full md:w-[1450px] mx-auto border'>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
